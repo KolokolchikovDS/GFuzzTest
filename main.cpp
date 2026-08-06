1894e632e682687094219e702fb49eb3582d5714
KolokolchikovDS <fatbahad@gmail.com>
2026-08-06 16:44:19 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "fuzztest/fuzztest.h"
+#include "gtest/gtest.h"
+
+#include <exception>
+
+
+void PassOnlyPositiveNumbers(int val)
+{
+    if (val < 0) {
+        throw std::runtime_error("TEST FAIL");
+    }
+}
+
+void MyFuzzTest(int input)
+{
+    SCOPED_TRACE(testing::Message() << "input = " << input);
+    ASSERT_NO_THROW(PassOnlyPositiveNumbers(input));
+}
+
+FUZZ_TEST(MyTestSuite, MyFuzzTest)
+.WithDomains(fuzztest::InRange(-100, -1));
